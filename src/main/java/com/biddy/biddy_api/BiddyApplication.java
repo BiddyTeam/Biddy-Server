38e5ec7e8979aa2dc6913b436c3dae12c310b814
dmori <chosehoon2@naver.com>
2025-08-08 14:44:42 +0900

    feat: 초기 설정

---
+++ src/main/java/com/biddy/biddy_api/BiddyApplication.java
@@ -0,0 +1,13 @@
+package com.biddy.biddy_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiddyApplication.class, args);
+	}
+
+}
