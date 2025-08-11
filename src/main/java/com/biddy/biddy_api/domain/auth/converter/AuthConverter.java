3eea0794ce369c4d57929237a542ad7beab38e72
dmori <chosehoon2@naver.com>
2025-08-11 17:12:29 +0900

    fix: 누락된 어노테이션 추가

--- src/main/java/com/biddy/biddy_api/domain/auth/converter/AuthConverter.java
+++ src/main/java/com/biddy/biddy_api/domain/auth/converter/AuthConverter.java
@@ -1,8 +1,11 @@
 package com.biddy.biddy_api.domain.auth.converter;
 
 import com.biddy.biddy_api.domain.auth.dto.KakaoLoginDTO;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AuthConverter {
+
     public KakaoLoginDTO.Response toKakaoLoginResponse(
             String accessToken, String refreshToken, Long id, String nickname, boolean isNewUser){
         return KakaoLoginDTO.Response.builder()
