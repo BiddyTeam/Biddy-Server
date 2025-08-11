31e0fe7d74922c83bac2d4cb8352aba6acae5ddb
Joon9750 <junhyeok2166@daum.net>
2025-08-11 16:43:45 +0900

    [Add] 경매 입찰 API 구현

--- src/main/java/com/biddy/biddy_api/domain/auction/BidController.java
+++ src/main/java/com/biddy/biddy_api/domain/auction/BidController.java
@@ -1,26 +1,48 @@
 package com.biddy.biddy_api.domain.auction;
 
 import com.biddy.biddy_api.domain.auction.dto.BidDto;
+import com.biddy.biddy_api.domain.auction.dto.PostBidRequest;
+import com.biddy.biddy_api.domain.auction.entity.Bid;
 import com.biddy.biddy_api.global.RspTemplate;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/bid")
 public class BidController {
 
     private final BidService bidService;
 
-    @GetMapping("auction/history")
-    public RspTemplate<List<BidDto>> getAuctionHistory(
+    @PostMapping()
+    @Operation(method = "Post", description = "입찰이 등록 되었습니다.")
+    public RspTemplate<Long> postBid(
+            @RequestBody PostBidRequest request
+    ) {
+        Long id = bidService.postBidInAuction(request);
+        return new RspTemplate<>(HttpStatus.CREATED, "입찰이 등록 되었습니다.", id);
+    }
+
+    @GetMapping("/auction/bids")
+    @Operation(method = "Get", description = "각 경매에 해당되는 입찰 이력을 조회합니다.")
+    public RspTemplate<List<BidDto>> getBidListInAuction(
+            @RequestParam Long auctionId
+    ) {
+        List<BidDto> list = bidService.getBidListInAuction(auctionId);
+        return new RspTemplate<>(HttpStatus.FOUND, "경매에 해당되는 입찰 이력을 조회합니다.", list);
+    }
+
+    @GetMapping("/history")
+    @Operation(method = "Get", description = "사용자가 입찰했던 이력을 조회합니다.")
+    public RspTemplate<List<BidDto>> getBidHistory(
             @RequestParam Long userId
     ) {
-        List<BidDto> list = bidService.getAuctionHistory(userId);
+        List<BidDto> list = bidService.getBidHistory(userId);
         return new RspTemplate<>(HttpStatus.FOUND, "입찰 이력을 조회합니다.", list);
     }
 }
--- src/main/java/com/biddy/biddy_api/domain/auction/BidService.java
+++ src/main/java/com/biddy/biddy_api/domain/auction/BidService.java
@@ -2,21 +2,51 @@ package com.biddy.biddy_api.domain.auction;
 
 import com.biddy.biddy_api.domain.auction.dto.AuctionDto;
 import com.biddy.biddy_api.domain.auction.dto.BidDto;
+import com.biddy.biddy_api.domain.auction.dto.PostBidRequest;
+import com.biddy.biddy_api.domain.auction.entity.Auction;
 import com.biddy.biddy_api.domain.auction.entity.Bid;
+import com.biddy.biddy_api.domain.auction.enums.BidStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class BidService {
 
     private final BidRepository bidRepository;
+    private final AuctionRepository auctionRepository;
 
-    public List<BidDto> getAuctionHistory(Long userId) {
+    public Long postBidInAuction(PostBidRequest request) throws RuntimeException {
+        Auction auction = auctionRepository.getAuctionById(request.getAuctionId()).orElseThrow();
+        Bid bid = Bid.builder()
+                .auction(auction)
+                .status(BidStatus.ACTIVE)
+                .amount(BigDecimal.valueOf(request.getAmount()))
+                .bidder(null) // 수정 필요
+                .isWinning(false)
+                .build();
+        Bid savedBid = bidRepository.save(bid);
+        return savedBid.getId();
+    }
+
+    public  List<BidDto> getBidListInAuction(Long auctionId) {
+        List<Bid> list = bidRepository.findAllByAuction(auctionId);
+        return toDtos(list);
+    }
+
+    public List<BidDto> getBidHistory(Long userId) {
         List<Bid> list = bidRepository.findAllByUserId(userId);
+        return toDtos(list);
+    }
+
+    private List<BidDto> toDtos(List<Bid> list) {
         List<BidDto> resultList = new ArrayList<>();
         for (Bid bid: list) {
             BidDto dto = BidDto.builder()
