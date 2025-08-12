26a9b2511094fb03a57aeeefa46a95a8b1ca096a
dmori <chosehoon2@naver.com>
2025-08-12 19:54:56 +0900

    feat: 경매 목록 조회 API 구현

--- src/main/java/com/biddy/biddy_api/domain/auction/controller/AuctionController.java
+++ src/main/java/com/biddy/biddy_api/domain/auction/controller/AuctionController.java
@@ -8,10 +8,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auctions")
@@ -20,11 +17,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuctionController {
 
     private final AuctionCommandService auctionCommandService;
+    private final AuctionQueryService auctionQueryService;
 
     @PostMapping
     @Operation(summary = "경매 게시글 등록", description = "새로운 경매 게시글을 등록합니다.")
     public RspTemplate<Long> createAuction(@RequestBody @Valid AuctionCreateDto.AuctionCreateRequest request) {
         Long auctionId = auctionCommandService.createAuction(request, 1L);
         return new RspTemplate<>(HttpStatus.CREATED, "경매가 등록되었습니다.", auctionId);
+    }
+
+    @GetMapping
+    @Operation(summary = "경매 목록 조회", description = "모든 경매 목록을 조회합니다.")
+    public RspTemplate<List<AuctionListDto>> getAllAuctions() {
+        List<AuctionListDto> auctions = auctionQueryService.getAllAuctions();
+        return new RspTemplate<>(HttpStatus.OK, "경매 목록을 조회했습니다.", auctions);
     }
 }
