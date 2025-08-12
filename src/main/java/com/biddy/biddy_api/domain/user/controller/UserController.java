d4a73322c3eeb4363173c30aeec951e7c10f5ed2
dmori <chosehoon2@naver.com>
2025-08-13 02:10:52 +0900

    feat: dto 구현

--- src/main/java/com/biddy/biddy_api/domain/user/controller/UserController.java
+++ src/main/java/com/biddy/biddy_api/domain/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.biddy.biddy_api.domain.user.controller;
 
 import com.biddy.biddy_api.domain.user.dto.MyPageProfileDto;
+import com.biddy.biddy_api.domain.user.dto.MyParticipatedAuctionDto;
 import com.biddy.biddy_api.domain.user.service.UserQueryService;
 import com.biddy.biddy_api.global.RspTemplate;
 import io.swagger.v3.oas.annotations.Operation;
@@ -11,6 +12,8 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
