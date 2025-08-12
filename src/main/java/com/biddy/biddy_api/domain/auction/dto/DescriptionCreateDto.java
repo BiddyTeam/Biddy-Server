b2a7700cda20fefd7544fce7b53b588c87f137b0
dmori <chosehoon2@naver.com>
2025-08-12 14:27:16 +0900

    feat: 경매 서비스 구현

--- src/main/java/com/biddy/biddy_api/domain/auction/dto/DescriptionCreateDto.java
+++ src/main/java/com/biddy/biddy_api/domain/auction/dto/DescriptionCreateDto.java
@@ -3,9 +3,12 @@ package com.biddy.biddy_api.domain.auction.dto;
 import com.biddy.biddy_api.domain.product.enums.ProductCategory;
 import com.biddy.biddy_api.domain.product.enums.ProductCondition;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Data;
 
 public class DescriptionCreateDto {
 
+    @Data
     @Schema(name = "DescritpionCreateRequest", description = "상품 설명 생성 요청")
     public static class Request {
         private String productName;
@@ -15,6 +18,8 @@ public class DescriptionCreateDto {
         private String userDescription;
     }
 
+    @Data
+    @Builder
     public static class Response {
         private String description;
         private Integer startPrice;
