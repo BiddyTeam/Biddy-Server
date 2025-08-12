8c856b7386a23e9ef04671a1e6f2583799db54a6
dmori <chosehoon2@naver.com>
2025-08-12 14:21:16 +0900

    feat: DTO 구현

--- src/main/java/com/biddy/biddy_api/domain/auction/dto/DescriptionCreateDto.java
+++ src/main/java/com/biddy/biddy_api/domain/auction/dto/DescriptionCreateDto.java
@@ -1,4 +1,24 @@
 package com.biddy.biddy_api.domain.auction.dto;
 
+import com.biddy.biddy_api.domain.product.enums.ProductCategory;
+import com.biddy.biddy_api.domain.product.enums.ProductCondition;
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public class DescriptionCreateDto {
+
+    @Schema(name = "DescritpionCreateRequest", description = "상품 설명 생성 요청")
+    public static class Request {
+        private String productName;
+        private ProductCategory productCategory;
+        private ProductCondition condition;
+        private Integer auctionPeriod;
+        private String userDescription;
+    }
+
+    public static class Response {
+        private String description;
+        private Integer startPrice;
+        private Integer endPrice;
+        private Integer buyNowPrice;
+    }
 }
